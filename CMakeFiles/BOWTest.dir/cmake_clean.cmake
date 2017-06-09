f2a5630e43fa126f7b8eef72ce2b9e60fc6ef2ec
Eli Simmonds <eli@carmelsoft.com>
2017-06-08 17:22:10 -0700

    first commit

---
+++ CMakeFiles/BOWTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BOWTest.dir/BOWTest.cpp.o"
+  "BOWTest.pdb"
+  "BOWTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BOWTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
